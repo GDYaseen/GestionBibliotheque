020ed20770a40808bc3d119c50aaaab66641f3bf
yassine <lahrigayassine@gmail.com>
2024-12-18 17:20:17 +0100

    Modifying borrow code

--- src/main/java/com/library/service/BorrowService.java
+++ src/main/java/com/library/service/BorrowService.java
@@ -1,25 +1,52 @@
 
 package com.library.service;
 
+import java.util.Date;
+
 import com.library.dao.BookDAO;
-import com.library.dao.StudentDAO;
-import com.library.model.Student;
+// import com.library.dao.StudentDAO;
+// import com.library.model.Student;
 import com.library.dao.BorrowDAO;
+import com.library.dao.StudentDAO;
 import com.library.model.Borrow;
+import com.library.model.Student;
+import com.library.model.Book;
 
 public class BorrowService {
 
     private BorrowDAO borrowDAO;
+    private BookDAO bookDAO;
 
     // Constructeur avec BorrowDAO
-    public BorrowService(BorrowDAO borrowDAO) {
+    public BorrowService(BorrowDAO borrowDAO,BookDAO bookDAO) {
         this.borrowDAO = borrowDAO;
+        this.bookDAO = bookDAO;
     }
 
     // Méthode pour emprunter un livre
-    public void borrowBook(Borrow borrow) {
+    public String borrowBook(int studentId,int bookId) {
+        Book b = bookDAO.getBookById(bookId);
+        if(b!=null && !b.isAvailable())
+            return "Le livre n'est pas disponible.";
+        Student s = new StudentDAO().getStudentById(studentId);
+        if(s==null || b==null)
+            return "Étudiant ou livre non trouvé.";
         // Sauvegarde de l'emprunt dans la base de données
-        borrowDAO.save(borrow);
+        Borrow borrow = new Borrow(studentId, bookId, new Date(), null);
+        bookDAO.markAvailable(bookId,false);
+        return borrowDAO.save(borrow);
+    }
+
+    public String returnBook(int studentId,int bookId) {
+        Book b = bookDAO.getBookById(bookId);
+        if(b!=null && b.isAvailable())
+            return "Le livre n'est pas emprunté.";
+        Student s = new StudentDAO().getStudentById(studentId);
+        if(s==null || b==null)
+            return "Étudiant ou livre non trouvé.";
+        // Sauvegarde de l'emprunt dans la base de données
+        bookDAO.markAvailable(bookId,true);
+        return borrowDAO.stopBorrow(studentId, bookId);
     }
 
     // Afficher les emprunts (méthode fictive, à adapter)
